faf8bf7ebbc9ebde5625cb4d40039d33c1ab72ff
TizianoN <hello@tizianonicolai.com>
2026-01-20 15:42:42 +0100

    add comments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // # INITIAL DATA
 
+// * images data
 const pics = [
   {
     image: "img/01.jpg",
@@ -28,14 +29,17 @@ const pics = [
   },
 ];
 
+// * index counter
 let currentActiveIndex = 0;
 
+// * autoplay config
 let autoplayIntervalId;
 let autoplayDirectionForward = true;
 const autoplayMs = 4000;
 
 // # HTML GENERATION
 
+// * retrieve existing nodes
 const carouselContainer = document.querySelector(".carousel-container");
 const nextButton = document.querySelector("#carousel #next");
 const prevButton = document.querySelector("#carousel #prev");
@@ -43,6 +47,7 @@ const invertButton = document.querySelector("#invert-direction");
 const galleryContainer = document.querySelector("#carousel .gallery");
 const thumbnailsContainer = document.querySelector("#thumbnails");
 
+// * gallery & thumbnails generazion
 let galleryHTML = "";
 let thumbnailsHTML = "";
 
@@ -71,8 +76,12 @@ for (let i = 0; i < pics.length; i++) {
 galleryContainer.innerHTML = galleryHTML;
 thumbnailsContainer.innerHTML = thumbnailsHTML;
 
+// * retrieve generated nodes
 const galleryNodes = document.querySelectorAll("#carousel .gallery figure");
 const thumbnailsNodes = document.querySelectorAll("#thumbnails .thumb");
+
+// * start carousel autoplay
+autoplayIntervalId = setAutoplay();
 
 // # EVENTS
 
@@ -93,6 +102,7 @@ prevButton.addEventListener("click", function () {
   goToIndex(currentActiveIndex - 1);
 });
 
+// * invert button click
 invertButton.addEventListener("click", function () {
   autoplayDirectionForward = !autoplayDirectionForward;
 });
@@ -104,7 +114,7 @@ document.addEventListener("keyup", function (e) {
   if (key === "ArrowRight") goToIndex(currentActiveIndex + 1);
 });
 
-// * pause autoplay
+// * container mouseenter - pause autoplay
 carouselContainer.addEventListener("mouseenter", function () {
   if (autoplayIntervalId) {
     clearInterval(autoplayIntervalId);
@@ -112,44 +122,49 @@ carouselContainer.addEventListener("mouseenter", function () {
   }
 });
 
-// * resume autoplay
+// * container mouseleave - resume autoplay
 carouselContainer.addEventListener("mouseleave", function () {
   if (!autoplayIntervalId) {
     autoplayIntervalId = setAutoplay();
   }
 });
 
-// # TIMING FUNCTIONS
-
-autoplayIntervalId = setAutoplay();
-
 // # FUNCTIONS
 
 function goToIndex(index) {
+  // * retrieve current index nodes
   const oldFigure = galleryNodes[currentActiveIndex];
   const oldThumb = thumbnailsNodes[currentActiveIndex];
 
+  // * remove class active - hides them
   oldFigure.classList.remove("active");
   oldThumb.classList.remove("active");
 
+  // * check if index in range - eventually correct
   if (index > galleryNodes.length - 1) index = 0;
   if (index < 0) index = galleryNodes.length - 1;
 
+  // * update index
   currentActiveIndex = index;
 
+  // * retrieve update index nodes
   const newFigure = galleryNodes[currentActiveIndex];
   const newThumb = thumbnailsNodes[currentActiveIndex];
 
+  // * add class active - shows them
   newFigure.classList.add("active");
   newThumb.classList.add("active");
 }
 
 function setAutoplay() {
+  // * returning setInterval id
   return setInterval(function () {
+    // * calculating newIndex by autoplay direction
     const newIndex = autoplayDirectionForward
       ? currentActiveIndex + 1
       : currentActiveIndex - 1;
 
+    // * going to newIndex
     goToIndex(newIndex);
   }, autoplayMs);
 }
